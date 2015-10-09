72f4544c80434641ca2f9de416b7fde010e24280
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2015-10-09 13:34:24 -0700

    Created a chart to represent the year 2012 for music media purchasing and integrated all the files together.

---
+++ Charts.js
@@ -0,0 +1,28 @@
+google.load('visualization', '1', {packages: ['corechart', 'bar']});
+google.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Music', 'Sales',],
+        ['Streaming', 1033],
+        ['Download', 2875],
+        ['Ringtone', 145],
+    ]);
+
+    var options = {
+        title: 'Music Sales of 2012',
+        chartArea: {width: '50%'},
+        hAxis: {
+            title: 'Music Sales 2012 (millions)',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Music Sales'
+        }
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
